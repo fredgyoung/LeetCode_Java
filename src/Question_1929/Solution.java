133d34172cd0cdd1480bc1ab1597297eec055524
Fred Young <fredgyoung@gmail.com>
2022-10-16 11:50:38 -0400

    added more solutions

---
+++ src/Question_1929/Solution.java
@@ -0,0 +1,12 @@
+package Question_1929;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+
+        int arrayLength = nums.length;
+        int[] result = new int[arrayLength * 2];
+        System.arraycopy(nums, 0, result, 0, arrayLength);
+        System.arraycopy(nums, 0, result, arrayLength, arrayLength);
+        return result;
+    }
+}
---
+++ src/Question_2235/Solution.java
@@ -0,0 +1,7 @@
+package Question_2235;
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}

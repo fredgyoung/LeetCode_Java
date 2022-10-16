d45a09b3ca2d30aa9477eb77ab66499bdd15bd4b
Fred Young <fredgyoung@gmail.com>
2022-10-16 11:14:56 -0400

    cleanup

--- src/Question_1/Solution.java
+++ src/Question_1/Solution.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int[] result = {0, 0};
+        int[] result = new int[2];
 
         HashMap<Integer, Integer> seen = new HashMap<>();
 
@@ -13,7 +13,6 @@ class Solution {
             int current = nums[i];
             int lookFor = target - current;
             if (seen.containsKey(lookFor)) {
-                // int result[] = {seen.get(lookFor), i};
                 result[0] = seen.get(lookFor);
                 result[1] = i;
                 break;

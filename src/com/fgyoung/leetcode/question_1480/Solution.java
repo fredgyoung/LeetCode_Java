bee0eb5881a134d4a7be5fcc3ab23279dab01044
Fred Young <fredgyoung@gmail.com>
2022-10-20 19:48:52 -0400

    added another solution

---
+++ src/com/fgyoung/leetcode/question_1480/Solution.java
@@ -0,0 +1,18 @@
+package com.fgyoung.leetcode.question_1480;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+        int result[] = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                result[i] = nums[i];
+            } else {
+                result[i] = nums[i] + result[i-1];
+            }
+        }
+
+        return result;
+    }
+}
